9305ff9de25ec0efe0b45c20e37d8925f93b1b9c
McMnyages <morachasilas@gmail.com>
2026-09-14 17:45:55 +0300

    feat: wrap App component with QueryClientProvider for React Query integration

--- bloglist/bloglist-frontend/src/main.jsx
+++ bloglist/bloglist-frontend/src/main.jsx
@@ -3,11 +3,15 @@ import App from './App'
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { NotificationContextProvider } from './NotificationContext'
+import { QueryClient,QueryClientProvider } from '@tanstack/react-query'
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <NotificationContextProvider>
     <Router>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </Router>
   </NotificationContextProvider>
 
