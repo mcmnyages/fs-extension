f6d90f2b2a967aa5bf7b3a7c90d10865b9484af6
McMnyages <morachasilas@gmail.com>
2026-09-15 19:52:06 +0300

    refactor: integrate user token retrieval and remove setToken function

--- bloglist/bloglist-frontend/src/services/blogs.js
+++ bloglist/bloglist-frontend/src/services/blogs.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
-let token = null
-const setToken = newToken => {
-  token = `Bearer ${newToken}`
-}
+import { getUser } from './persistentUser'
+const user = getUser()
+
+let token = `Bearer ${user.token}`
 
 const getAll = () => {
   const request = axios.get(baseUrl)
@@ -38,7 +38,6 @@ const deleteBlog = async (id) => {
 export default {
   getAll,
   create,
-  setToken,
   updateBlog,
   deleteBlog
 }
